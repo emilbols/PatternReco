0d893e7dade96c9d683615deef9daa889a4d8683
Emil Bols <emilbols@hotmail.com>
2020-10-08 11:27:28 +0200

    Adding a video capture class, which we can thread, so we can record while moving the stages

--- test.py
+++ test.py
@@ -7,15 +7,77 @@ import ctypes
 from ctypes import windll, c_double
 #from ctypes import windll, c_double, create_string_buffer
 import sys, time
-
+import os.path
 import numpy as np
 from edge_finder import edge_find, rho_theta_to_xy, select_lines,average_over_nearby_lines,distance_between_lines, corner_find
-cap = cv2.VideoCapture(0)
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,2560);
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,2560);
-cap.set(cv2.CAP_PROP_FPS,25);
+class VideoWriterWidget(object):
+    def __init__(self, video_file_name, src=0):
+        # Create a VideoCapture object
+        self.frame = 0
+        self.frame_name = 'cam_output'+str(src)
+        self.video_file = video_file_name
+        self.video_file_name = video_file_name + '.avi'
+        self.capture = cv2.VideoCapture(src,cv2.CAP_V4L )
+        self.capture.set(cv2.CAP_PROP_FRAME_WIDTH,2560)
+        self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT,2560)
+		self.capture.set(cv2.CAP_PROP_FPS,25)
 
+        # Default resolutions of the frame are obtained (system dependent)
+        self.frame_width = int(self.capture.get(3))
+        self.frame_height = int(self.capture.get(4))
+
+        # Set up codec and output video settings
+        #self.codec = cv2.VideoWriter_fourcc('M','J','P','G')
+        #self.output_video = cv2.VideoWriter(self.video_file_name, self.codec, 30, (self.frame_width, self.frame_height))
+
+        # Start the thread to read frames from the video stream
+        self.thread = Thread(target=self.update, args=())
+        self.thread.daemon = True
+        self.thread.start()
+
+        # Start another thread to show/save frames
+        self.start_recording()
+        print('initialized {}'.format(self.video_file))
+
+    def update(self):
+        # Read the next frame from the stream in a different thread
+        while True:
+            if self.capture.isOpened():
+                (self.status, self.frame) = self.capture.read()
+
+
+    def show_frame(self):
+        # Display frames in main program
+        if self.status:
+            cv2.imshow(self.frame_name, self.frame)
+
+        # Press Q on keyboard to stop recording
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            self.capture.release()
+            self.output_video.release()
+            cv2.destroyAllWindows()
+            exit(1)
+
+    def save_frame(self):
+        # Save obtained frame into video output file
+        self.output_video.write(self.frame)
+
+    def start_recording(self):
+        # Create another thread to show/save frames
+        def start_recording_thread():
+            while True:
+                try:
+                    self.show_frame()
+                    #self.save_frame()
+                except AttributeError:
+                    pass
+        self.recording_thread = Thread(target=start_recording_thread, args=())
+        self.recording_thread.daemon = True
+        self.recording_thread.start()
+
+video_feed = VideoWriterWidget('Camera_1', 0)
 
 xComPort=5
 yComPort=7
@@ -31,11 +93,7 @@ zDistance=0.0
 nExport=0
 z_value=0.0
 
-# np is an alias pointing to numpy library
-import numpy as np
-# capture frames from a camera
 
-import os.path
 dll_name = "ps10.dll"
 dllabspath = os.path.dirname(os.path.abspath(__file__)) + os.path.sep + dll_name
 # load library
@@ -137,10 +195,6 @@ while(xstate > 0):
     print( "Position=%.3f" %(xreadout) )
     # reads frames from a camera
     xstate = mydll.PS10_GetMoveState(xPS, nAxis) 
-    ret, frame = cap.read()
-    cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
-    cv2.imshow('Original',frame)
-    cv2.resizeWindow('Original', 1024,800)
 
 
 xreadout=PS10_GetPositionEx(xPS, nAxis)
@@ -161,10 +215,7 @@ while(ystate > 0):
     print( "Position=%.3f" %(yreadout) )
     # reads frames from a camera
     ystate = mydll.PS10_GetMoveState(yPS, nAxis) 
-    ret, frame = cap.read()
-    cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
-    cv2.imshow('Original',frame)
-    cv2.resizeWindow('Original', 1024,800)
+
 
     
 time.sleep(5)
@@ -179,10 +230,7 @@ while(xstate > 0):
     print( "Position=%.3f" %(xreadout) )
     # reads frames from a camera
     xstate = mydll.PS10_GetMoveState(xPS, nAxis) 
-    ret, frame = cap.read()
-    cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
-    cv2.imshow('Original',frame)
-    cv2.resizeWindow('Original', 1024,800)
+
 
 
 xreadout=PS10_GetPositionEx(xPS, nAxis)
@@ -202,10 +250,7 @@ while(ystate > 0):
     print( "Position=%.3f" %(yreadout) )
     # reads frames from a camera
     ystate = mydll.PS10_GetMoveState(yPS, nAxis) 
-    ret, frame = cap.read()
-    cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
-    cv2.imshow('Original',frame)
-    cv2.resizeWindow('Original', 1024,800)
+
 
     
 time.sleep(5)
@@ -220,10 +265,7 @@ while(zstate > 0):
     print( "Position=%.3f" %(zreadout) )
     # reads frames from a camera
     zstate = mydll.PS10_GetMoveState(zPS, nAxis)
-    ret, frame = cap.read()
-    cv2.namedWindow('Original',cv2.WINDOW_NORMAL)
-    cv2.imshow('Original',frame)
-    cv2.resizeWindow('Original', 1024,800)
+
 
 
 
