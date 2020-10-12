f65183a6ae5956b3214f73986cab6d3a73c5a097
Emil Bols <emilbols@hotmail.com>
2020-10-12 14:32:49 +0200

    add method for selecting line pairs, and calculating the distance between them

--- still_images.py
+++ still_images.py
@@ -3,7 +3,7 @@ import cv2
 import os
 import math
 import glob
-
+from copy import deepcopy
 import matplotlib.pyplot as plt
 from edge_finder import edge_find, corner_find, line, xy_intersection, check_perpendicular, cv2HoughLines, average_over_nearby_lines, process_image, distance_between_lines, select_lines, select_line_pairs
 
@@ -14,19 +14,18 @@ if not os.path.exists(dir_name):
     os.mkdir(dir_name)
 
 full_files = [i.split('/')[-1] for i in glob.glob(input_dir+"*.jpg")]
-
 for filename in full_files:
+    #filename = 'side1x0.95y80.0.jpg'
     the_file = os.path.join(input_dir, filename)
     name = filename.split('.j')[0]
     output_name = dir_name+name
     
     img = cv2.imread(the_file)
     #img = cv2.imread(the_file,0)
-    edges, lines_img, threshold = process_image(img)
+    edges, lines_img, threshold, lines, scanned_lines, distances = process_image(img)
     cv2.imwrite(output_name+'_threshold.jpg',threshold)
     cv2.imwrite(output_name+'_edges.jpg',edges)
     cv2.imwrite(output_name+'_lines.jpg',lines_img)
-
 
 ## Fin
 
